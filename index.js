3229f0e05104e21333c17cc0a8d31d4f6ee24272
Peter-Van-Drunen <peetevd@gmail.com>
2017-08-18 13:18:57 -0400

    implemented basic regex

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const express = require('express')  
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.use((request, response, next) => {  
-  console.log(request.headers)
-  next()
-})
+app.use(bodyParser.json());
 
-app.use((request, response, next) => {  
-  request.chance = Math.random()
-  next()
-})
-
-app.get('/', (request, response) => {  
-  response.json({
-    chance: request.chance
-  })
-})
+app.get('/:regex/:subject', function (req, res) {
+ 
+    const rgx = new RegExp(req.params.regex , 'g');
+ 
+    var response = [];
+    var arr;
+    while ((arr = rgx.exec(req.params.subject)) !== null) {
+        response.push(arr[0]);
+    }   
+ 
+    res.json(response);
+});
+ 
+app.post('/regex', function (req, res) {
+    const rgx = new RegExp(req.body.source, 'g');
+ 
+    var response = [];
+    var arr;
+    while ((arr = rgx.exec(req.body.subject)) !== null) {
+        response.push(arr[0]);
+    }
+ 
+    res.json(response);
+});
 
 app.listen(1337);
